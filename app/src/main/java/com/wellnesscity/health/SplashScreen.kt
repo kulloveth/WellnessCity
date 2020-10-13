8697b4504557c342f0e9d48af12c953048fa4ca1
osaiol <59148635+osaiol@users.noreply.github.com>
2020-10-13 14:01:07 +0100

    initial commit

---
+++ app/src/main/java/com/wellnesscity/health/SplashScreen.kt
@@ -0,0 +1,60 @@
+package com.wellnesscity.health
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+import java.util.logging.Handler
+
+class SplashScreen : AppCompatActivity() {
+
+private val SPLASH_DELAY: Long = 1000
+    private var mDelayHandler: android.os.Handler? = null
+    private var progressBarStatus = 0
+    var dummy: Int = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+       mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+      private fun launchMainActivity() {
+          var intent = Intent(this, MainActivity::class.java)
+          intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+          startActivity(intent)
+          this.finish()
+          mDelayHandler!!.removeCallbacks(mRunnable)
+
+    }
+
+    private val mRunnable: Runnable = Runnable {
+        Thread(Runnable {
+            while (progressBarStatus < 100) {
+                try {
+                    dummy = dummy + 25
+                    Thread.sleep(100)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+                progressBarStatus = dummy
+                progress_bar.progress = progressBarStatus }
+            launchMainActivity()
+                }).start()
+            }
+
+    override fun onDestroy() {
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+        super.onDestroy()
+    }
+
+}
