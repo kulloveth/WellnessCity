100b4f48c1fce2ef7b8864e3eb2c1fab8ae72091
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2020-10-16 13:06:54 +0100

    changes to the SplashFragment

--- app/src/main/java/com/wellnesscity/health/ui/intro/SplashFragment.kt
+++ app/src/main/java/com/wellnesscity/health/ui/intro/SplashFragment.kt
@@ -1,10 +1,15 @@
 package com.wellnesscity.health.ui.intro
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.wellnesscity.health.R
 import com.wellnesscity.health.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,7 +17,11 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class SplashFragment : Fragment() {
     private var binding: FragmentSplashBinding? = null
-
+    private val handler = Handler()
+    private val runnable = Runnable {
+        requireView().findNavController()
+            .navigate(R.id.action_splashFragment_to_onboardingFragment)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,4 +32,18 @@ class SplashFragment : Fragment() {
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable,3000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
 }
