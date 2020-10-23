2a39ed7309df2220db1d3ecb766f5610ef724a1d
kulloveth <kulloveth@gmail.com>
2020-10-23 10:39:47 +0100

    create feature activity

---
+++ app/src/main/java/com/wellnesscity/health/ui/activity/FeatureActivity.kt
@@ -0,0 +1,12 @@
+package com.wellnesscity.health.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wellnesscity.health.R
+
+class FeatureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature)
+    }
+}
