0ce66977faf2f5ee2745633e6fade935c394b34a
kulloveth <kulloveth@gmail.com>
2020-10-24 14:33:38 +0100

    add image url to json file

--- app/src/main/java/com/wellnesscity/health/ui/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/wellnesscity/health/ui/welcome/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import coil.api.load
 import com.wellnesscity.health.databinding.FragmentWelcomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,5 +24,8 @@ class WelcomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //binding?.barIv?.load("https://firebasestorage.googleapis.com/v0/b/wellnesscity-efcc6.appspot.com/o/illness%2Farthritis.jpg?alt=media&token=e5499ddf-fcda-44ad-9315-c383ee33e0f4")
     }
+
+
 }
