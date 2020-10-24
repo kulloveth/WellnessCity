287ae3a365def04b5aaa37654162d9b588fca3b1
kulloveth <kulloveth@gmail.com>
2020-10-24 12:19:43 +0100

    parse json data

--- app/src/main/java/com/wellnesscity/health/data/FirebaseService.kt
+++ app/src/main/java/com/wellnesscity/health/data/FirebaseService.kt
@@ -1,6 +1,7 @@
 package com.wellnesscity.health.data
 
 import android.content.Context
+import androidx.annotation.RawRes
 import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.JsonReader
 import com.squareup.moshi.Moshi
@@ -14,14 +15,39 @@ import javax.annotation.Resource
 
 class FirebaseService(private val context: Context) {
 
-    fun readJsonFile(resource: Resource, fileName: String) {
+    init {
+        readJsonFile(R.raw.illness, "conditions_with_symptoms")
+    }
+
+    fun readJsonFile(@RawRes resource: Int, fileName: String) {
         val jsonString =
-            context.resources.openRawResource(R.raw.illness).bufferedReader().use { it.readText() }
+            context.resources.openRawResource(resource).bufferedReader().use { it.readText() }
         val jsonObject = JSONObject(jsonString)
         val illnessData = jsonObject.getJSONArray(fileName)
+
         for (i in 0 until illnessData.length()) {
-            val JsonObject = illnessData.getJSONObject(i)
+            val  illnessArray = illnessData.getJSONObject(i)
+            val id = illnessArray.getInt("id")
+            val condition = illnessArray.getString("condition")
+            val img = illnessArray.getString("img_url")
+            val desc = illnessArray.getString("desc")
+            val treatment = illnessArray.getString("treatment")
+            val symptomsList = mutableListOf<String>()
+            val symptomsArray = illnessArray.getJSONArray("symptoms")
+            for (i in 0 until symptomsArray.length()){
+                val symptoms = symptomsArray.getString(i)
+                symptomsList.add(symptoms) }
+            val causesList = mutableListOf<String>()
+            val causesArray = illnessArray.getJSONArray("causes")
+            for (i in 0 until causesArray.length()){
+                val causes = causesArray.getString(i)
+                causesList.add(causes) }
+
+            val conditionsWithSymptoms = ConditionsWithSymptom(treatment,causesList,condition,desc,id,img,symptomsList)
+            Timber.d("$conditionsWithSymptoms")
         }
+
+
     }
 
     companion object {
