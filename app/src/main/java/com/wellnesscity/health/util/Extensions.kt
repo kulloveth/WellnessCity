4ed101b2a9f83b3d146958ec6cc6351d3ad4f88b
kulloveth <kulloveth@gmail.com>
2020-10-25 14:57:58 +0100

    change ui

---
+++ app/src/main/java/com/wellnesscity/health/util/Extensions.kt
@@ -0,0 +1,22 @@
+package com.wellnesscity.health.util
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.View
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import com.wellnesscity.health.R
+
+
+fun View.setMenuBackgroundColor(context: Context, @ColorInt color: Int){
+    val unwrappedDrawable = AppCompatResources.getDrawable(context, R.drawable.menu_bg)
+    val wrappedDrawable = DrawableCompat.wrap(unwrappedDrawable!!)
+    DrawableCompat.setTint(wrappedDrawable, color)
+}
+
+fun Drawable.tint(context: Context, @ColorRes color: Int) {
+    DrawableCompat.setTint(this, ContextCompat.getColor(context, color))
+}
