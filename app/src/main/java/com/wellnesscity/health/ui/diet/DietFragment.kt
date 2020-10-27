5a07ad2925a7c1f41080d42ac25ff070e7afb15e
kulloveth <kulloveth@gmail.com>
2020-10-27 23:22:35 +0100

    display diets

--- app/src/main/java/com/wellnesscity/health/ui/diet/DietFragment.kt
+++ app/src/main/java/com/wellnesscity/health/ui/diet/DietFragment.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.wellnesscity.health.R
+import com.wellnesscity.health.data.model.Status
+import com.wellnesscity.health.databinding.FragmentDietBinding
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
 @AndroidEntryPoint
 class DietFragment : Fragment() {
     private val viewModel:DietViewModel by viewModels()
-
+private var binding:FragmentDietBinding? = null
+    private val adapter = DietAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
@@ -25,10 +29,39 @@ class DietFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+       binding = FragmentDietBinding.inflate(layoutInflater)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showData()
+    }
+    fun showData(){
         viewModel.fetchRecipeData("Ketogenic").observe(requireActivity(), Observer {
-            Timber.d("${it.data?.results?.size}")
+            when(it.status) {
+                Status.SUCCESS-> {
+                    binding?.shimmerFl?.stopShimmerAnimation()
+                     binding?.rv?.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+                    binding?.rv?.adapter = adapter
+                    adapter.submitList(it.data?.results)
+                    Timber.d("${it.data}")
+                    binding?.shimmerFl?.visibility = View.GONE
+                    binding?.rv?.visibility = View.VISIBLE
+                    Timber.d("${it.data?.results?.size}")
+                }
+            }
         })
-        return inflater.inflate(R.layout.fragment_diet, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding?.shimmerFl?.startShimmerAnimation()
+    }
+
+    override fun onPause() {
+        binding?.shimmerFl?.stopShimmerAnimation()
+        super.onPause()
     }
 
 }
